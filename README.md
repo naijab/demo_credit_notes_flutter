d6dda62a3a460d2dafa01081dac9448ef82537ce
Nattapon Pondongnok <nainatjab999@gmail.com>
2021-12-01 12:17:50 +0700

    init project flutter

---
+++ README.md
@@ -0,0 +1,5 @@
+# demo_credit_notes
+
+> Credit Notes Demo App with flutter (cross-platform - Android, iOS)
+
+
---
+++ android/app/src/main/kotlin/com/naijab/DemoCreditNote/demo_credit_notes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.naijab.DemoCreditNote.demo_credit_notes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
